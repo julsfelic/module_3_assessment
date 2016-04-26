5f79aa17cb909c2c3d2d8967bf0783716a6a96c0
Julian Feliciano <julsfelic@gmail.com>
2016-04-26 11:03:16 -0600

    Build up query for longDescription
    Currently in the process of figuring out the query for searching by longDescription

--- spec/features/user_can_search_for_headphones_spec.rb
+++ spec/features/user_can_search_for_headphones_spec.rb
@@ -23,4 +23,27 @@ RSpec.feature "User can search for headphones" do
       end
     end
   end
+
+  context "when searching for sennheiser headphones white" do
+    scenario "they see limited results" do
+      VCR.use_cassette("user_search_headphones_with_color") do
+        visit root_path
+
+        fill_in "search_name", with: "sennheiser headphones white"
+        click_on "search"
+
+        expect(current_path).to eq("/search")
+
+        results = all('.results li')
+
+        expect(results.length).to eq 3
+        expect(results[0]).to have_content("2602352")
+        expect(results[0]).to have_content("Sennheiser - Active Noise-Canceling Headphones - Black")
+        expect(results[0]).to have_content("2.7")
+        expect(results[0]).to have_content("From our expanded online assortment; TalkThrough function; NoiseGard Digital noise cancellation; 10mm drivers")
+        expect(results[0]).to have_content("229.99")
+        expect(results[0]).to have_css("img[src='http://img.bbystatic.com/BestBuy_US/images/products/2602/2602352_sa.jpg']")
+      end
+    end
+  end
 end
