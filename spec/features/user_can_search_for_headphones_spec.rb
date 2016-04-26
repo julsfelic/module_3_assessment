27cb4db4135f47a26cd4e3ab23d18b6c215f2f8c
Julian Feliciano <julsfelic@gmail.com>
2016-04-26 10:17:18 -0600

    Work through first user story spec
    Finishing up the first user story spec

--- spec/features/user_can_search_for_headphones_spec.rb
+++ spec/features/user_can_search_for_headphones_spec.rb
@@ -14,8 +14,12 @@ RSpec.feature "User can search for headphones" do
         results = all('li')
 
         expect(results.length).to eq 15
-
-        # Finish expectations 
+        expect(results[0]).to have_content("2602352")
+        expect(results[0]).to have_content("Sennheiser - Active Noise-Canceling Headphones - Black")
+        expect(results[0]).to have_content("2.7")
+        expect(results[0]).to have_content("From our expanded online assortment; TalkThrough function; NoiseGard Digital noise cancellation; 10mm drivers")
+        expect(results[0]).to have_content("229.99")
+        expect(results[0]).to have_css("img[src='http://img.bbystatic.com/BestBuy_US/images/products/2602/2602352_sa.jpg']")
       end
     end
   end
