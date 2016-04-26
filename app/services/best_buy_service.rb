bde9a1894abff45bbaf2ec60356d54b9b521ba9f
Julian Feliciano <julsfelic@gmail.com>
2016-04-26 10:09:24 -0600

    Create BestBuyService PORO
    Created a PORO to hit the Best Buy API and return headphones based off
    search and limit it to 15 responses

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -7,5 +7,16 @@ class BestBuyService
   end
 
   def headphones(params)
+    parse(connection.get("/v1/products((search=#{params[:name]})&(categoryPath.id=abcat0204000))"))
   end
+
+  private
+
+    def connection
+      @_connection
+    end
+
+    def parse(response)
+      JSON.parse(response.body).deep_symbolize_keys
+    end
 end
